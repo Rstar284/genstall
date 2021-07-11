ad0bd80929a3af6fd99c0134ab1f0287266d2c73
Rstar284 <56640404+Rstar284@users.noreply.github.com>
2021-07-11 21:24:50 +0400

    Update 99-final.sh

--- genstall.d/99-final.sh
+++ genstall.d/99-final.sh
@@ -10,13 +10,16 @@ echo "have a nice time with your new gentoo install :)"
 cd /
 umount -l /mnt/gentoo/dev{/shm,/pts,} || true
 umount -l /mnt/gentoo{/boot,/proc,} || true
-read -p "Reboot now? Pressing enter will select yes by default" -n 1 -r response
+read -p "Reboot now?[Y/n] Pressing enter will also select yes by default" -n 1 -r response
 echo
 response=${response,,} # tolower
  if [[ $response =~ ^(Y|y| ) ]] || [[ -z $response ]]; then
     reboot now
+elif [[ $response =~ ^(N|n)]]; then
+    echo "You pressed no; Exiting Gentoo Install Script, Bye!"
+    exit 1 && return 1
 else
-    echo "Exiting Gentoo Install Script, Bye!"
+    echo "Invalid Option"
     exit 1 && return 1
 fi
     
