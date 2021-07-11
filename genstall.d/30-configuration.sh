bb6338ffa89b916a59cc2b282413776807c81f48
Rstar284 <56640404+Rstar284@users.noreply.github.com>
2021-07-11 22:07:02 +0400

    Update 30-configuration.sh

--- genstall.d/30-configuration.sh
+++ genstall.d/30-configuration.sh
@@ -14,9 +14,9 @@ $_CHROOT locale-gen
 
 cat > /mnt/gentoo/etc/fstab << EOF
 # <fs>        <mountpoint>        <type>        <opts>            <dump/pass>
-/dev/$DRIVE_NAME1     /boot               $BOOT_FS      noauto,noatime    0 2
-/dev/$DRIVE_NAME2     none                swap          sw                0 0
-/dev/$DRIVE_NAME3     /                   $ROOT_FS      defaults,noatime  0 1
+/dev/${DRIVE_NAME}1     /boot               $BOOT_FS      noauto,noatime    0 2
+/dev/${DRIVE_NAME}2     none                swap          sw                0 0
+/dev/${DRIVE_NAME}3     /                   $ROOT_FS      defaults,noatime  0 1
 EOF
 
 cat > /mnt/gentoo/etc/conf.d/hostname << EOF
@@ -31,8 +31,8 @@ EOF
 
 $_CHROOT /bin/bash << EOF
 cd /etc/init.d
-ln -s net.lo net.$ETH0
-rc-update add net.$ETH0 default
+ln -s net.lo net.${ETH0}
+rc-update add net.${ETH0} default
 EOF
 
 $_CHROOT passwd root << EOF
