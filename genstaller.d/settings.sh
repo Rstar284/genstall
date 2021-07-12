69bc912cb5637daf03533a2e87b5d8d29812ef06
Rstar284 <supermanrehan284@gmail.com>
2021-07-12 12:02:18 +0400

    thing

--- genstaller.d/settings.sh
+++ genstaller.d/settings.sh
@@ -30,7 +30,7 @@ DAEMONS="syslog-ng cronie sshd"
 _CORES=$(($(nproc) + 1))
 _LATEST_STAGE3=$(curl -s $DIST_MIRROR/releases/amd64/autobuilds/latest-stage3-amd64.txt | tail -1 | awk '{print $1}')
 _STAGE3_URI="$DIST_MIRROR/releases/amd64/autobuilds/$_LATEST_STAGE3"
-_CHROOT="chroot /bin/bash /mnt/gentoo"
+_CHROOT="chroot /mnt/gentoo /bin/bash"
 function _EMERGE() {
 	set +e
 	$_CHROOT emerge --pretend "$@"
